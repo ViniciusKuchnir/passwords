a0fda68524c5c868b313723243cd01ef82b70d46
Vinícius Kuchnir <vina200902@gmail.com>
2022-08-06 15:40:29 -0300

    Criação da interface Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,70 +1,114 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { Text, View, Image, TouchableOpacity, StyleSheet } from "react-native";
 import Slider from "@react-native-community/slider";
-
+import { generatePassword } from "../../utils/generatePassword";
+import { Feather } from "@expo/vector-icons";
+import * as Clipboard from "expo-clipboard";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
 
 export default function Main() {
-    const [characters, setCharacters] = useState(2);
- return (
+  const [characters, setCharacters] = useState(2);
+  const [utility, setUtility] = useState(null);
+  const [password, setPassword] = useState("");
+  
+  const copyToClipboard = async () => {
+    await Clipboard.setStringAsync(password);
+  };
+
+  function sendForm(){
+    console.log(password);
+  }
+
+  return (
     <View style={styles.container}>
-    <StatusBar style="auto" />
-    <View style={styles.content}>
-      <Image source={require("../../assets/img/icon-noneBackground.png")} />
-      <Text style={styles.characters}>Caracteres: {characters}</Text>
-      <Slider
-        style={styles.slider}
-        minimumValue={2}
-        maximumValue={20}
-        step={2}
-        thumbTintColor="#232323"
-        minimumTrackTintColor="#FFAC33"
-        maximumTrackTintColor="#232323"
-        onValueChange={setCharacters}
-        value={characters}
-      />
-      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
-        <Text style={styles.button__text}>Gerar Senha</Text>
-      </TouchableOpacity>
+      <StatusBar style="auto" />
+      <View style={styles.content}>
+        <Input label="Qual será a utilidade da senha?" value={utility} setValue={setUtility} />
+        <Text style={styles.characters}>Caracteres: {characters}</Text>
+        <Slider
+          style={styles.slider}
+          minimumValue={2}
+          maximumValue={20}
+          step={2}
+          thumbTintColor="#232323"
+          minimumTrackTintColor="#FFAC33"
+          maximumTrackTintColor="#232323"
+          onValueChange={setCharacters}
+          value={characters}
+        />
+
+        <Button text="Gerar senha" onPress={() => generatePassword(characters, password, setPassword)}/>
+        {password !== "" ? (
+          <View style={styles.result}>
+            <View style={styles.password}>
+              <Text style={styles.result__text}>{password}</Text>
+              <TouchableOpacity
+                style={styles.clipboard}
+                onPress={() => copyToClipboard()}
+              >
+                <Feather name="clipboard" size={32} color="#232323" />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.button}>
+              <Button text="Salvar senha" onPress={() => sendForm()}/>
+            </View>
+          </View>
+        ) : (
+          <View style={styles.result} />
+        )}
+      </View>
     </View>
-  </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 20,
-      backgroundColor: "#FFF",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    content: {
-      width: "100%",
-      justifyContent: "center",
-      alignItems: "center",
-    },
-    characters: {
-      fontSize: 24,
-      fontWeight: "bold",
-      color: "#232323",
-    },
-    slider: {
-      width: "100%",
-      height: 48,
-    },
-    button: {
-      width: "50%",
-      height: 48,
-      justifyContent: "center",
-      alignItems: "center",
-      borderRadius: 48 / 2,
-      backgroundColor: "#FFAC33",
-    },
-    button__text: {
-      fontSize: 16,
-      fontWeight: "bold",
-      color: "#FFF",
-      letterSpacing: 0.5,
-    },
-  });
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#FFF",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  content: {
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  characters: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#232323",
+  },
+  slider: {
+    width: "100%",
+    height: 48,
+  },
+  button: {
+    width: "50%",
+    marginLeft: "auto",
+    marginRight: "auto",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  result: {
+    height: 48,
+    width: "100%",
+  },
+  password:{
+    width: "100%",
+    height: "100%",
+    marginTop: 60,
+    paddingStart: 10,
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  result__text: {
+    flex: 1,
+    fontSize: 22,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
